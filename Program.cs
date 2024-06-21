766645fe8c847a313b9569325add254e5e0055ee
chiwauwa <chiwauwa@DESKTOP-CS7FDCL>
2024-06-21 21:51:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace bebra
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Hello, World!");
+            Console.Write("Привіт світ!");
+            
+        }
+    }
+}
